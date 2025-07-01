053c61208583da56460767f66d457adba500547d
ahmadmujahid <ahmadmujahid343@gmail.com>
2025-07-02 00:53:09 +0800

    feat: start using zustand

---
+++ app/products/[id]/page.tsx
@@ -0,0 +1,11 @@
+import { ProductDetail } from "@/components/product-detail";
+import { stripe } from "@/lib/stripe";
+import React from "react";
+
+export default async function ProductPage({params,}:{params: {id: string};}) {
+	const product = await stripe.products.retrieve(params.id, {
+		expand: ["default_price"],
+	});
+
+	return <ProductDetail product={product}/>;
+}
