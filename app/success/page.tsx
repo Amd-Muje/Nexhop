f1ca11a33f9fdaebcb8d8cfc1ef2dafbe6d1c316
ahmadmujahid <ahmadmujahid343@gmail.com>
2025-07-02 12:12:50 +0800

    fix: style error

--- app/success/page.tsx
+++ app/success/page.tsx
@@ -1,22 +1,23 @@
-"use client"
+"use client";
 
 import { useCartStore } from "@/store/cart-store";
 import Link from "next/link";
 import { useEffect } from "react";
 
 export default function SuccessPage() {
-    const { clearCart } = useCartStore();
-    useEffect (() => {
-        clearCart();
-    }, [clearCart]);
-
-    return (
-        <div>
-            <h1>Payment Succesfull!</h1>
-            <p>Thank You for your purchase, your order is being processed</p>
-        <Link href={'/products'}>
-            continue shopping
-        </Link>
-        </div>
-    );
+  const { clearCart } = useCartStore();
+  useEffect(() => {
+    clearCart();
+  }, [clearCart]);
+  return (
+    <div className="container mx-auto px-4 py-8 text-center">
+      <h1 className="text-3xl font-bold mb-4">Payment Successful!</h1>
+      <p className="mb-4">
+        Thank you for your purchase. Your order is being processed.
+      </p>
+      <Link href="/products" className="text-blue-600 hover:underline">
+        Continue Shopping
+      </Link>
+    </div>
+  );
 }
